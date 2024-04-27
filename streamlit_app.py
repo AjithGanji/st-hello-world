f3ef7737aa15c4c11efbf59ef6e377369bcebb62
AjithGanji <124641053+AjithGanji@users.noreply.github.com>
2024-04-27 17:40:01 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -39,4 +39,4 @@ with open("test.html", 'r', encoding='utf-8') as index_file:
 
 # Display HTML content along with CSS
 # components.html(full_html_content_with_css, height=1000, scrolling=True)
-components.html(html_content, height=1000, scrolling=True)
+components.html(html_content, height=2000, scrolling=True)
